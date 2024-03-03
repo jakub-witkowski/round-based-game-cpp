c5b0b8c654fe6d6f841fd0191779721d374a7eef
jakub.witkowski <j.witkowski@poczta.onet.pl>
2024-03-03 14:39:09 +0100

    Class TBase added

---
+++ headers/TBase.h
@@ -0,0 +1,23 @@
+#ifndef TBASE_H
+#define TBASE_H
+
+#include "../headers/TUnit.h"
+#include "../headers/TMap.h"
+
+#include <iostream>
+
+class TBase : public TUnit
+{
+    public:
+        TBase();
+        TBase(char, TMap*);
+        TBase(char,
+                unsigned int,
+                unsigned int,
+                unsigned int,
+                unsigned int);
+
+        ~TBase();
+};
+
+#endif
---
+++ sources/TBase.cpp
@@ -0,0 +1,60 @@
+#include "../headers/TBase.h"
+
+TBase::TBase()
+{
+
+}
+
+TBase::TBase(char aff, TMap* m) : TUnit()
+{
+    set_affiliation(aff);
+    set_type('B');
+
+    /* determine coordinates */
+    if (aff == 'P')
+        set_coordinates(0, 0);
+    else if (aff == 'E')
+        set_coordinates(m->get_map_size_x() - 1, m->get_map_size_y() - 1);
+    
+    /* determine id */
+    if (aff == 'P')
+        set_id(1);
+    else if (aff == 'E')
+        set_id(2);
+
+    set_stamina(200);
+    set_speed(0);
+    set_cost(0);
+    set_attack_range(0);
+    set_training_time(0);
+
+    update_unit_counter();
+
+    std::cout << "2-argument constructor called" << std::endl;
+}
+
+TBase::TBase(char aff,
+                 unsigned int x,
+                 unsigned int y,
+                 unsigned int id,
+                 unsigned int stm)
+{
+    set_affiliation(aff);
+    set_type('B');
+    set_coordinates(x,y);
+    set_id(id);
+    set_stamina(stm);
+    set_speed(0);
+    set_cost(0);
+    set_attack_range(0);
+    set_training_time(0);
+
+    update_unit_counter();
+
+    std::cout << "5-argument constructor called" << std::endl;
+}
+
+TBase::~TBase()
+{
+
+}
